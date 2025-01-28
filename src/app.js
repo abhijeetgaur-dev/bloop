f9cd69ae602a28312c07a4fa54fdeb5d9df50008
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2025-01-28 14:28:23 +0530

    update files

--- src/app.js
+++ src/app.js
@@ -2,3 +2,13 @@ const express =  require('express');
 
 const app = express();
 
+
+app.use("/test", (req,res) =>{
+    res.send('Hello Test')
+})
+
+app.use("/",(req,res) =>{
+    res.send('Hello World')
+})
+
+app.listen(3000)
