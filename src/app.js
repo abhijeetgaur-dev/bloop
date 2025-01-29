a54146d4f0f98e5cb387fa998e8de96a7fd2378c
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2025-01-29 23:19:35 +0530

    testing api

--- src/app.js
+++ src/app.js
@@ -3,12 +3,27 @@ const express =  require('express');
 const app = express();
 
 
+app.get("/user", (req,res) =>{
+    res.send({name: "John", age: 30});
+})
+
+app.post("/user", (req,res)=>{
+    res.send("user added");
+})
+
+app.delete("/user", (req,res) =>{
+    res.send('User deleted');
+})
+
 app.use("/test", (req,res) =>{
     res.send('Hello Test')
 })
+
+
 
 app.use("/",(req,res) =>{
     res.send('Hello World')
 })
 
 app.listen(3000)
+console.log("listening on port 3000")
