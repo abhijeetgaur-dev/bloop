78a4f96d3bddc9b8ab6454d7acd172e7bbebf906
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2025-02-04 14:25:27 +0530

    saving data in DB

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const app = express();
 const {connectDB} = require("./config/database")
+const User  = require("./models/user")
 // const {auth, userAuth} = require("./middlewares/auth")
+
+
+
+app.post("/signup" , async (req,res)=>{
+    const user = new User ({
+        firstName : "kyunki",
+        lastName: "gaur",
+        emailId : "abhijeetgaur@gmail.com",
+        password : "hello",
+        gender: "male"
+    });
+
+    await user.save();
+
+    res.send("user added successfully");
+})
 
 connectDB()
     .then(()=>{
