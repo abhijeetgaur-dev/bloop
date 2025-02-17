a2b021e251007ef5d6f02f01fe9591d101a5980c
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2025-02-18 00:41:07 +0530

    creating connection request model

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const connectionRequestSchema =  new mongoose.Schema({
+  fromUserId :{
+    type: mongoose.Schema.Types.ObjectId,
+    required : true
+  },
+  toUserId : {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  status:{
+    type: String,
+    enum : {
+      values : ["interested", "ignored", "accepted", "rejected"],
+      message: `{VALUES} is incorrect type!`
+    },
+    required: true
+  }
+},{
+  timestamps: true
+});
+
+const ConnectionRequest = new mongoose.model('ConnectionRequestModel', connectionRequestSchema);
+
+module.exports = ConnectionRequest;
